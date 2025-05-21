443f6cf10158f1e30eca34ae73183761d42947f4
hantopic <parkws8821@gmail.com>
2025-05-21 15:49:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -23,60 +23,58 @@ def search_pubmed(query, max_results=100):
     BATCH_SIZE = 200
     for i in range(0, len(ids), BATCH_SIZE):
         batch_ids = ids[i:i + BATCH_SIZE]
-
         fetch_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
         fetch_params = {
-        "db": "pubmed",
-        "id": ",".join(ids),
-        "retmode": "xml"
-    }
-            fetch_resp = requests.get(fetch_url, params=fetch_params)
-            soup = BeautifulSoup(fetch_resp.content, "xml")
+            "db": "pubmed",
+            "id": ",".join(batch_ids),
+            "retmode": "xml"
+        }
+        fetch_resp = requests.get(fetch_url, params=fetch_params)
+        soup = BeautifulSoup(fetch_resp.content, "xml")
 
-    results = []
-            for article in soup.find_all("PubmedArticle"):
-        try:
-            title = article.ArticleTitle.text.strip()
-            authors = ", ".join([
-                f"{a.LastName.text} {a.Initials.text}"
-                for a in article.find_all("Author")
-                if a.LastName and a.Initials
-            ])
-            journal = article.Article.Journal.Title.text if article.Article.Journal and article.Article.Journal.Title else ""
-            year = article.Article.Journal.JournalIssue.PubDate.Year.text if article.Article.Journal.JournalIssue.PubDate.Year else ""
-            volume = article.Article.Journal.JournalIssue.Volume.text if article.Article.Journal.JournalIssue.Volume else ""
-            issue = article.Article.Journal.JournalIssue.Issue.text if article.Article.Journal.JournalIssue.Issue else ""
-            pages = article.Article.Pagination.MedlinePgn.text if article.Article.Pagination and article.Article.Pagination.MedlinePgn else ""
-            article_id = article.MedlineCitation.PMID.text
-            article_number = ""
-            for aid in article.ArticleIdList.find_all("ArticleId"):
-                if aid["IdType"] == "doi":
-                    article_number = aid.text
-            abstract = article.Article.Abstract.AbstractText.text if article.Article.Abstract and article.Article.Abstract.AbstractText else ""
+        for article in soup.find_all("PubmedArticle"):
+            try:
+                title = article.ArticleTitle.text.strip()
+                authors = ", ".join([
+                    f"{a.LastName.text} {a.Initials.text}"
+                    for a in article.find_all("Author")
+                    if a.LastName and a.Initials
+                ])
+                journal = article.Article.Journal.Title.text if article.Article.Journal and article.Article.Journal.Title else ""
+                year = article.Article.Journal.JournalIssue.PubDate.Year.text if article.Article.Journal.JournalIssue.PubDate.Year else ""
+                volume = article.Article.Journal.JournalIssue.Volume.text if article.Article.Journal.JournalIssue.Volume else ""
+                issue = article.Article.Journal.JournalIssue.Issue.text if article.Article.Journal.JournalIssue.Issue else ""
+                pages = article.Article.Pagination.MedlinePgn.text if article.Article.Pagination and article.Article.Pagination.MedlinePgn else ""
+                article_id = article.MedlineCitation.PMID.text
+                article_number = ""
+                for aid in article.ArticleIdList.find_all("ArticleId"):
+                    if aid["IdType"] == "doi":
+                        article_number = aid.text
+                abstract = article.Article.Abstract.AbstractText.text if article.Article.Abstract and article.Article.Abstract.AbstractText else ""
 
-            keywords = []
-            for kwlist in article.find_all("KeywordList"):
-                for kw in kwlist.find_all("Keyword"):
-                    keywords.append(kw.text)
-            keyword_str = ", ".join(keywords)
+                keywords = []
+                for kwlist in article.find_all("KeywordList"):
+                    for kw in kwlist.find_all("Keyword"):
+                        keywords.append(kw.text)
+                keyword_str = ", ".join(keywords)
 
-            pdf_link = f"https://pubmed.ncbi.nlm.nih.gov/{article_id}/"
+                pdf_link = f"https://pubmed.ncbi.nlm.nih.gov/{article_id}/"
 
-            results.append({
-                "title": title,
-                "authors": authors,
-                "journal": journal,
-                "year": year,
-                "volume": volume,
-                "issue": issue,
-                "pages": pages if pages else article_number,
-                "pmid": article_id,
-                "pdf_link": pdf_link,
-                "keywords": keyword_str,
-                "summary": summarize_text(abstract)
-            })
-        except Exception:
-            continue
+                results.append({
+                    "title": title,
+                    "authors": authors,
+                    "journal": journal,
+                    "year": year,
+                    "volume": volume,
+                    "issue": issue,
+                    "pages": pages if pages else article_number,
+                    "pmid": article_id,
+                    "pdf_link": pdf_link,
+                    "keywords": keyword_str,
+                    "summary": summarize_text(abstract)
+                })
+            except Exception:
+                continue
 
     return results
 
